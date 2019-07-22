df5bef164068948ff516650cbb32293d93d51d7c
jameswang <jameswangdr@gmail.com>
2019-07-22 12:08:00 -0700

    Added Schema

---
+++ models/Recipe.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const User = require('./User');
+
+const RecipeSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    ingredients: [
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            amount: {
+                type: String,
+                required: true,
+            },
+        }
+    ],
+    procedure: {
+        type: String,
+        required: true,
+    },
+    User: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+});
+
+const Recipe = mongoose.model('Recipe', RecipeSchema);
+
+module.exports = Recipe;
