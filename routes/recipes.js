ce4263f3340484f79a2398bcda40f60df22862a3
meganecummings <megcummings@gmail.com>
2019-07-24 15:33:08 -0700

    Updated Show Functionality to Id

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ctrl = require('../controllers');
 
 router.get('/', ctrl.recipeCtrl.index);
-router.get('/:name', ctrl.recipeCtrl.show);
+router.get('/:_id', ctrl.recipeCtrl.show);
 router.post('/', ctrl.recipeCtrl.new);
 router.post('/:_id', ctrl.recipeCtrl.update);
 router.delete('/:_id', ctrl.recipeCtrl.delete);
